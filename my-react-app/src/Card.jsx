7f33e4aaa4eef43e3dc62031b67fb9ee476c34c5
rusualin <rusualin0031@gmail.com>
2024-03-19 21:52:21 +0100

    styling .card

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,7 +3,7 @@
 function Card() {
     return(
         <div className="card">
-            <img src="https://images.pexels.com/photos/280249/pexels-photo-280249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="london image"></img>
+            <img className="card-image" src="https://images.pexels.com/photos/280249/pexels-photo-280249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="london image"></img>
             <h1>Card</h1>
             <p>Big Ben London</p>
         </div>
