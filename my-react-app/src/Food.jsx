86fd15292cf0d75cdd0fd5eeffd8ab80d48e4b0b
rusualin <rusualin0031@gmail.com>
2024-03-19 18:00:28 +0100

    create file Food.jsx + function + const food1 and food2 + create order list for fruit

--- my-react-app/src/Food.jsx
+++ my-react-app/src/Food.jsx
@@ -1,2 +1,17 @@
 
 
+function Food() {
+
+    const food1 = "Kiwi";
+    const food2 = "Mango";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food
