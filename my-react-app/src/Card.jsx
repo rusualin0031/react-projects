2be9cde70d436489850f8196495516a83dfd57ed
rusualin <rusualin0031@gmail.com>
2024-03-21 10:57:23 +0100

    create folder ToDoList

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,7 +3,7 @@
 function Card() {
     return(
         <div className="card">
-            <img className="card-image" src="https://images.pexels.com/photos/280249/pexels-photo-280249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="london image"></img>
+            <img className="card-image" src="https://images.pexels.com/photos/258117/pexels-photo-258117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="london image"></img>
             <h1 className="card-title">Card</h1>
             <p className="card-text">Big Ben London</p>
         </div>
