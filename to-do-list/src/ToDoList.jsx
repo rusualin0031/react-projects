e3e7133c47b1a62257af75f11f3cda55e12a0453
rusualin <rusualin0031@gmail.com>
2024-03-21 17:49:32 +0100

    create const newTask

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function ToDoList() {
 
     const [tasks, setTask] = useState([]);
+    const [newTask, setNewTask] = useState("");
 
     return(<></>);
 }
